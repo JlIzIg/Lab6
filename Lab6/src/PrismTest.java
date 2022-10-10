85306d16b1ff2d741f53bcb1cd2e58244558cf77
JlIzIg <113330694+JlIzIg@users.noreply.github.com>
2022-10-10 10:04:52 +0300

    Update PrismTest.java

--- Lab6/src/PrismTest.java
+++ Lab6/src/PrismTest.java
@@ -1,31 +1,36 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class PrismTest {
 
 
-    public ArrayList<Prism> getPrisms() {
+    public List<Prism> getPrisms() {
         return prisms;
     }
-    public void setPrisms(ArrayList<Prism> prisms) {
+
+    public void setPrisms(List<Prism> prisms) {
         this.prisms = prisms;
     }
-    public ArrayList<Prism> prisms;
+
+    public List<Prism> prisms;
+
     public PrismTest() {
         this.prisms = new ArrayList<>();
     }
-    public boolean addPrism(Prism prism)
-    {
+
+    public boolean addPrism(Prism prism) {
         return prisms.add(prism);
     }
-    public void removeOnIndex(int index)
-    {
-        if (index<prisms.size())
+
+    public void removeOnIndex(int index) {
+        if (index < prisms.size())
             prisms.remove(index);
     }
-    public void removeOnElement(Prism prism)
-    {
+
+    public void removeOnElement(Prism prism) {
         prisms.remove(prism);
     }
+
     public int getMaxDiagonal() {
         int maxIndex = 0;
         for (int i = 0; i < prisms.size() - 1; i++) {
@@ -33,7 +38,7 @@ public class PrismTest {
                 maxIndex = i + 1;
             }
         }
-        return  maxIndex;
+        return maxIndex;
     }
 }
 
